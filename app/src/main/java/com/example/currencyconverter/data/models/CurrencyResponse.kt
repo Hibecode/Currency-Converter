9705fafa713d20d75e79ea2297fad7b2585969dd
Hibecode <ibkokunoye@gmail.com>
2022-02-15 16:52:54 +0100

    Resource and Repo added

--- app/src/main/java/com/example/currencyconverter/data/models/CurrencyResponse.kt
+++ app/src/main/java/com/example/currencyconverter/data/models/CurrencyResponse.kt
@@ -1,5 +1,7 @@
 package com.example.currencyconverter.data.models
 
+
+
 data class CurrencyResponse(
     val base: String,
     val date: String,
--- app/src/main/java/com/example/currencyconverter/di/AppModule.kt
+++ app/src/main/java/com/example/currencyconverter/di/AppModule.kt
@@ -1,12 +1,13 @@
 
 
 package com.example.currencyconverter.di
+
+
 /*
-
-
 import android.app.Application
 import com.example.currencyconverter.api.CurrencyApi
 import com.example.currencyconverter.data.models.CurrencyResponse
+import com.example.currencyconverter.util.Constants.Companion.BASE_URL
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -17,7 +18,7 @@ import retrofit2.create
 import javax.inject.Singleton
 
 
-private const val BASE_URL = "http://data.fixer.io/api/"
+//private const val BASE_URL = "http://data.fixer.io/api/"
 
 @Module
 @InstallIn(ApplicationComponent::class)
@@ -30,5 +31,6 @@ object AppModule {
         .addConverterFactory(GsonConverterFactory.create())
         .build()
         .create(CurrencyApi::class.java)
-}
-*/
+
+
+}*/
