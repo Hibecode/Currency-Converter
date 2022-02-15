f3a3f6e03309f92cd218fa63ad1d572aa89a3c6d
Hibecode <ibkokunoye@gmail.com>
2022-02-15 15:16:46 +0100

    AppModule object added

---
+++ app/src/main/java/com/example/currencyconverter/di/AppModule.kt
@@ -0,0 +1,28 @@
+
+package com.example.currencyconverter.di
+
+/*
+
+import android.app.Application
+import com.example.currencyconverter.data.models.CurrencyApi
+import com.example.currencyconverter.data.models.CurrencyResponse
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+
+private const val BASE_URL = "http://data.fixer.io/api/"
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideCurrencyApi(): CurrencyApi = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .build()
+}*/
